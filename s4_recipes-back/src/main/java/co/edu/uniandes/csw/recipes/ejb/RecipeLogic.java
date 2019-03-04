38e642fa698ec02cf997092aa22746940a8dbf1d
estudiante <estudiante@ISIS2603-423.sis.virtual.uniandes.edu.co>
2019-03-04 17:53:06 -0500

    Implementacion de la logica validando las redes de negocio

--- s4_recipes-back/src/main/java/co/edu/uniandes/csw/recipes/ejb/RecipeLogic.java
+++ s4_recipes-back/src/main/java/co/edu/uniandes/csw/recipes/ejb/RecipeLogic.java
@@ -6,7 +6,10 @@
 package co.edu.uniandes.csw.recipes.ejb;
 
 import co.edu.uniandes.csw.recipes.entities.RecipeEntity;
+import co.edu.uniandes.csw.recipes.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.recipes.persistence.RecipePersistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -16,6 +19,9 @@ import javax.inject.Inject;
  */
 @Stateless
 public class RecipeLogic {
+    
+     private static final Logger LOGGER = Logger.getLogger(RecipeLogic.class.getName());
+
     @Inject
     private RecipePersistence persistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
 
@@ -24,9 +30,25 @@ public class RecipeLogic {
     }
 
     //TODO crear el método createRecipe
-    public RecipeEntity createRecipe()
+    public RecipeEntity createRecipe(RecipeEntity entity) throws BusinessLogicException
     {
+        LOGGER.log(Level.INFO, "Inicia proceso de creación de una receta");
+        if(entity.getName() == null|| entity.getName().isEmpty() || entity.getName().length() <= 30)
+        {
+            throw new BusinessLogicException("El nombre de la receta no es valido");
+        }
         
+        if(persistence.findByName(entity.getName()) != null)
+        {
+            throw new BusinessLogicException("Ya existe una receta con dicho nombre");
+        }
+        
+        if(entity.getName() == null|| entity.getName().isEmpty() || entity.getName().length() <= 150)
+        {
+            throw new BusinessLogicException("La descripcion de la receta no es valida");
+        }
+        
+        return persistence.createRecipe(entity);
     }
 
 }
